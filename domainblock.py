ab3e988421a5801441a7eec475651631bf1bf842
Wayland Morgan <dotwayland@gmail.com>
2018-02-12 08:24:55 -0600

    Update domainblock.py

--- domainblock.py
+++ domainblock.py
@@ -5,7 +5,6 @@ import logging
 import httplib as http_client
 import re
 
-zone = str("rpz-security.local")
 def block_domain(domain, comment):
     url = 'https://%(server)s/wapi/v2.2.2/record:cname' % conf
     headers = {'Content-Type': 'application/json'}
@@ -62,51 +61,56 @@ def confirm(msg):
     a = raw_input(msg + "? [y/n] ")
     return a.lower() in ('y','yes','ok')
 
-logf = open("/services/service-user/var/log/rpz.log", "a")
-requester = os.environ['TTY_OWNER']
-conf = read_config()
+def main():
+    logf = open("/services/service-user/var/log/rpz.log", "a")
+    requester = os.environ['TTY_OWNER']
+    conf = read_config()
 
-#Get domain from Analyst
-domain = input_domain_to_block()
-subdomain = "*."+domain
-blocksubdomain = False
-if not domain.startswith("*"):
-    if is_domain_already_blocked(subdomain):
-        print "[*] Note: "+subdomain+" is already blocked."
-    elif confirm("[?] Do you also want to block "+subdomain):
-        blocksubdomain = True
+    #Get domain from Analyst
+    domain = input_domain_to_block()
+    subdomain = "*."+domain
+    blocksubdomain = False
+    if not domain.startswith("*"):
+        if is_domain_already_blocked(subdomain):
+            print "[*] Note: "+subdomain+" is already blocked."
+        elif confirm("[?] Do you also want to block "+subdomain):
+            blocksubdomain = True
 
-#Get Jira ticket number and comment from Analyst
-jira = input_matches_regex("Enter Jira ticket # (e.g., IR-123456): ", '^[a-zA-Z]{3}\-\d{6}$', 'Please enter a valid Jira ticket #')
-extra_comment = input_matches_regex("Enter a comment:")
-comment = "Blocked by %s for %s: %s" % (requester, jira, extra_comment)
-print "[*] The following comment will be recorded in IPAM: '%s'" % comment
+    #Get Jira ticket number and comment from Analyst
+    jira = input_matches_regex("Enter Jira ticket # (e.g., IR-123456): ", '^[a-zA-Z]{3}\-\d{6}$', 'Please enter a valid Jira ticket #')
+    extra_comment = input_matches_regex("Enter a comment:")
+    comment = "Blocked by %s for %s: %s" % (requester, jira, extra_comment)
+    print "[*] The following comment will be recorded in IPAM: '%s'" % comment
 
-if not confirm("Does everything look correct?"):
-    print "[*] Ok, exiting"
-    sys.exit(0)
+    if not confirm("Does everything look correct?"):
+        print "[*] Ok, exiting"
+        sys.exit(0)
 
-#Make the call to IPAM
-try:
-    debug_api()
-    response = block_domain(domain, comment)
-    timestr = time.strftime("%Y-%m-%dT%H:%M:%S")
-    print "\n{noformat}"
-    print "[*] The domain "+domain+" was sinkholed at "+timestr+" in response to this incident.\n"
-    logf.write("[succeeded] {0} {1} {2} {3} {4} \n".format(timestr, domain, response.status_code, response.reason, comment))
-
-    if blocksubdomain:
+    #Make the call to IPAM
+    try:
         debug_api()
-        response = block_domain(subdomain, comment)
+        response = block_domain(domain, comment)
         timestr = time.strftime("%Y-%m-%dT%H:%M:%S")
-        print "[*] The domain "+subdomain+" was sinkholed at "+timestr+" in response to this incident."
-        print "{noformat}\n"
-        logf.write("[succeeded] {0} {1} {2} {3} {4} \n".format(timestr, subdomain, response.status_code, response.reason, comment))
+        print "\n{noformat}"
+        print "[*] The domain "+domain+" was sinkholed at "+timestr+" in response to this incident.\n"
+        logf.write("[succeeded] {0} {1} {2} {3} {4} \n".format(timestr, domain, response.status_code, response.reason, comment))
 
-#Catch HTTP error codes and log them 
-except requests.exceptions.HTTPError as err:
-    status_code = err.response.status_code
-    status_reason = err.response.reason
-    print "[!!] The server responded with code and reason", status_code, status_reason
-    logf.write("{0} [{1}] {2} {3} {4} {5} [failed]\n".format(timestr, jira, domain, requester, status_code, status_reason))
-    sys.exit(-1)
+        if blocksubdomain:
+            debug_api()
+            response = block_domain(subdomain, comment)
+            timestr = time.strftime("%Y-%m-%dT%H:%M:%S")
+            print "[*] The domain "+subdomain+" was sinkholed at "+timestr+" in response to this incident."
+            print "{noformat}\n"
+            logf.write("[succeeded] {0} {1} {2} {3} {4} \n".format(timestr, subdomain, response.status_code, response.reason, comment))
+
+    #Catch HTTP error codes and log them 
+    except requests.exceptions.HTTPError as err:
+        status_code = err.response.status_code
+        status_reason = err.response.reason
+        print "[!!] The server responded with code and reason", status_code, status_reason
+        logf.write("{0} [{1}] {2} {3} {4} {5} [failed]\n".format(timestr, jira, domain, requester, status_code, status_reason))
+        sys.exit(-1)
+        
+if __name__ == '__main__':
+    zone = str("rpz-security.local")
+    main()
